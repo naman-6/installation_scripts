590510a622015c1171b4df6d5c84c4b1b0c7030b
NAMAN <104288359+naman-6@users.noreply.github.com>
2024-04-03 13:20:18 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
 sudo apt update
-sudo apt install fontconfig openjdk-17-jre
+sudo apt install fontconfig openjdk-17-jre -y
 
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
@@ -9,4 +9,4 @@ echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get install jenkins -y
