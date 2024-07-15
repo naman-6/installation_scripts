7578cfd69e0329b9d6f1d54bbd9483cf1cfffb7b
NAMAN <104288359+naman-6@users.noreply.github.com>
2024-07-15 11:58:03 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -15,3 +15,5 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo usermod -aG docker $USER
+sudo chown $USER //var/run/docker.sock
