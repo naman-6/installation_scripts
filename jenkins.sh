5206e35f505f6f5d0824dc26fc5a9b12a4341600
NAMAN <104288359+naman-6@users.noreply.github.com>
2026-02-07 02:21:52 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,12 +1,14 @@
 #!bin/bash
 
 sudo apt update
-sudo apt install fontconfig openjdk-17-jre -y
+sudo apt install fontconfig openjdk-21-jre
+java -version
 
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
-  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
-sudo apt-get install jenkins -y
+sudo apt update
+sudo apt install jenkins -y
